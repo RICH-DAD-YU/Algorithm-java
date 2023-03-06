80f0550a51cc4d23c1583c4aeabe6f28741c6706
dokyung <dbehrud9323@hanmail.net>
2023-03-06 22:10:10 +0900

    add algorithm

---
+++ Java/BJ_10951_A더하기B.java
@@ -0,0 +1,17 @@
+package bj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class A더하기B_10951 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		System.out.println(a+b);
+	}
+}
